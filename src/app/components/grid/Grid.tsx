cb8d8ba231b1481ae2cf0c508355f30465760959
jahlela <jahlela5@gmail.com>
2025-07-10 11:54:54 +0200

    Add buttons to select a full column

--- src/app/components/grid/Grid.tsx
+++ src/app/components/grid/Grid.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { useState } from 'react';
-
 import { Cell } from '../cell';
 import styles from './grid.module.scss';
 
 type Grid = { isComplete: boolean };
+const GRID_CELLS: number[] = [...Array(81)];
 
-const getRandomSudokuNumber = () => Math.floor(Math.random() * 9) + 1;
+const getColumnCells = (col: number) => Array.from({ length: 9 }, (_, row) => row * 9 + col);
+const getRowCells = (row: number) => Array.from({ length: 9 }, (_, col) => row * 9 + col);
 
 export const Grid = ({ isComplete = true }: Grid) => {
   const [selectedCells, setSelectedCells] = useState<number[]>([]);
@@ -16,22 +17,36 @@ export const Grid = ({ isComplete = true }: Grid) => {
     <div className={styles.wrapper}>
     {isComplete && <div className={styles.overlay}>✔ Complete!</div>}
 
-    <div className={styles.grid}>
-      {[...Array(81)].map((_, i) => {
-        const isSelected = selectedCells.includes(i);
-        const handleClick = () => {
-          if (isSelected) {
-            setSelectedCells(selectedCells.filter(cell => cell !== i));
-          } else {
-            setSelectedCells([...selectedCells, i]);
-          }
-        };
+        {/* buttons to select all cells in the column below */}
+        {[...Array(9)].map((_, col) => (
+          <button
+            key={col}
+            className={styles.columnButton}
+            onClick={() => {
+              const columnCells = getColumnCells(col);
+              setSelectedCells(columnCells);
+            }}
+          >
+            {col + 1}
+          </button>
+        ))} 
 
-        return (
+      <div className={styles.grid}>
+        {GRID_CELLS.map((_, i) => {
+          const isSelected = selectedCells.includes(i);
+          const handleClick = () => {
+            if (isSelected) {
+              setSelectedCells(selectedCells.filter(cell => cell !== i));
+            } else {
+              setSelectedCells([...selectedCells, i]);
+            }
+          };
+
+          return (
           <Cell
             key={i}
             index={i}
-            value={getRandomSudokuNumber()}
+            value={i+1}
             isSelected={isSelected}
             handleClick={handleClick}
           />
